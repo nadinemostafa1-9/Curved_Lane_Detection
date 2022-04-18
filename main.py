9f12d37115337a5db16b8b1d19a0ac6d4d04c304
NouranHussein <70723581+NouranHussein@users.noreply.github.com>
2022-04-18 23:28:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -132,18 +132,6 @@ def sliding_windown(img_w):
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
 
-    # Generate x and y values for plotting
-    # ploty = np.linspace(0, img_w.shape[0] - 1, img_w.shape[0])
-    # left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-    # right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
-    #
-    # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-    # plt.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
 
     return left_fit, right_fit
 
