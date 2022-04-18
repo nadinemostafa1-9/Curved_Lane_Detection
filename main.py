18aeba784995d325a99b3e66265fd15d930e8697
Nadine Mostafa <Nadine.mostafa19@gmail.com>
2022-04-18 23:16:33 +0200

    add image_binary function

--- main.py
+++ main.py
@@ -5,6 +5,63 @@ from moviepy.editor import VideoFileClip
 from IPython.display import HTML
 
 
+def image_binary(img, sobel_kernel=9, mag_thresh=(10, 255), s_thresh=(170, 255)):
+
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    gray = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+
+
+    # Binary matrixes creation
+    sobel_binary = np.zeros(shape=gray.shape, dtype=bool)
+    s_binary = sobel_binary
+    combined_binary = s_binary.astype(np.float32)
+
+    # Sobel Transform
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = 0 #cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    sobel_abs = np.abs(sobelx**2 + sobely**2)
+    sobel_abs = np.uint8(255 * sobel_abs / np.max(sobel_abs))
+
+    sobel_binary[(sobel_abs > mag_thresh[0]) & (sobel_abs <= mag_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+
+    # Combine the two binary thresholds
+
+    combined_binary[(s_binary == 1) | (sobel_binary == 1)] = 1
+    combined_binary = np.uint8(255 * combined_binary / np.max(combined_binary))
+
+    # ---------------- MASKED IMAGE --------------------
+    offset = 100
+    mask_polyg = np.array([[(0 + offset, img.shape[0]),
+                            (img.shape[1] / 2.5, img.shape[0] / 1.65),
+                            (img.shape[1] / 1.8, img.shape[0] / 1.65),
+                            (img.shape[1], img.shape[0])]],
+                          dtype=np.int)
+
+    # Next we'll create a masked edges image using cv2.fillPoly()
+    mask_img = np.zeros_like(combined_binary)
+    ignore_mask_color = 255
+
+    # This time we are defining a four sided polygon to mask
+    # Applying polygon
+    cv2.fillPoly(mask_img, mask_polyg, ignore_mask_color)
+    masked_edges = cv2.bitwise_and(combined_binary, mask_img)
+
+    return masked_edges
+
+
+def warp(img, src, dst):
+    src = np.float32([src])
+    dst = np.float32([dst])
+
+    return cv2.warpPerspective(img, cv2.getPerspectiveTransform(src, dst),
+                               dsize=img.shape[0:2][::-1], flags=cv2.INTER_LINEAR)
+
+
 def sliding_windown(img_w):
 
     histogram = np.sum(img_w[int(img_w.shape[0] / 2):, :], axis=0)
