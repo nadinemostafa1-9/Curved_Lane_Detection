45bf591258a3f1b314803e92c97437851c95dc98
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2022-05-16 20:16:40 +0200

    Update carDetection.py

--- carDetection.py
+++ carDetection.py
@@ -1,3 +1,197 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+from scipy.ndimage import label
+from moviepy.editor import VideoFileClip
+from collections import deque
+import pickle
+import os
+import queue
+# Define a function to return some characteristics of the dataset
+def data_look(car_list, notcar_list):
+    data_dict = {}
+    # Define a key in data_dict "n_cars" and store the number of car images
+    data_dict["n_cars"] = len(car_list)
+    # Define a key "n_notcars" and store the number of notcar images
+    data_dict["n_notcars"] = len(notcar_list)
+    # Read in a test image, either car or notcar
+    car_img = mpimg.imread(car_list[0])
+    # Define a key "image_shape" and store the test image shape 3-tuple
+    data_dict["image_shape"] = car_img.shape
+    # Define a key "data_type" and store the data type of the test image.
+    data_dict["data_type"] = car_img.dtype
+    # Return data_dict
+    return data_dict
+
+
+def visualize_images(input_images, num_cols, figure_name, cmap=None):
+    "Shows input images by stacking them in num_cols columns"
+    fig, axes = plt.subplots((int)((len(input_images) + 1) / num_cols), num_cols, figsize=(24, 20))
+
+    fig = plt.gcf()
+    fig.canvas.set_window_title(figure_name)
+
+    print(figure_name)
+
+    for ax, image in zip(axes.flat, input_images):
+        if (cmap == "gray" or cmap == 'hot'):
+            cv2.imshow('output',image)
+            cv2.waitKey(0)
+        elif (image.shape[2] == 1):
+            cv2.imshow('output',image)
+            cv2.waitKey(0)
+        else:
+            cv2.imshow('output',image)
+            cv2.waitKey(0)
+
+
+# Read cars and not-cars images
+
+# Data folders
+vehicles_dir = 'vehicles/'
+non_vehicles_dir = 'non-vehicles/'
+
+# images are divided up into vehicles and non-vehicles
+cars = []
+notcars = []
+
+# Read vehicle images
+images = glob.iglob(vehicles_dir + '/**/*.png', recursive=True)
+
+for image in images:
+    cars.append(image)
+
+# Read non-vehicle images
+images = glob.iglob(non_vehicles_dir + '/**/*.png', recursive=True)
+
+for image in images:
+    notcars.append(image)
+
+data_info = data_look(cars, notcars)
+
+print('Your function returned a count of',
+      data_info["n_cars"], ' cars and',
+      data_info["n_notcars"], ' non-cars')
+
+
+
+def convert_color(img, conv='RGB2YCrCb'):
+    """
+    Convert the image from one color space to the other
+    """
+    if conv == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conv == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conv == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    if conv == 'RGB2HLS':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    if conv == 'RGB2HSV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    if conv == 'Gray':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if conv == 'RGB2YUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+
+
+# HOG features of image
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,
+                     vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient,
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block),
+                                  transform_sqrt=False,
+                                  visualize=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient,
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block),
+                       transform_sqrt=False,
+                       visualize=vis, feature_vector=feature_vec)
+        return features
+
+
+def bin_spatial(img, size=(16, 16)):
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
+
+# Define a function to compute color histogram features
+# NEED TO CHANGE bins_range if reading .png files with mpimg!
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
+                        hist_bins=32, orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel='ALL',
+                        spatial_feat=False, hist_feat=False, hog_feat=True):
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        file_features = []
+        # Read in each one by one
+        image = mpimg.imread(file)
+        image = (image * 255).astype(np.uint8)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif cspace == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else: feature_image = np.copy(image)
+
+        if spatial_feat == True:
+            spatial_features = bin_spatial(feature_image, size=spatial_size)
+            file_features.append(spatial_features)
+        if hist_feat == True:
+            # Apply color_hist()
+            hist_features = color_hist(feature_image, nbins=hist_bins)
+            file_features.append(hist_features)
+        if hog_feat == True:
+        # Call get_hog_features() with vis=False, feature_vec=True
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.append(get_hog_features(feature_image[:,:,channel],
+                                    orient, pix_per_cell, cell_per_block,
+                                    vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)
+
+            # Append the new feature vector to the features list
+            file_features.append(hog_features)
+        features.append(np.concatenate(file_features))
+    # Return list of feature vectors
+    return features
+
 #do it only once to extract features and save it in pickle file as it takes times
 def train_svc():
  colorspace = 'YUV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
